3ab98e08386b26ce0efb06bd16220e958da566b5
AlexLeoChumak <chumak1106@gmail.com>
2024-07-27 20:57:03 +0300

    create HeaderComponent

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'health-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
