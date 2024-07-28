d0b35f5827038c75448dbf9086d1a42c530b7c68
AlexLeoChumak <chumak1106@gmail.com>
2024-07-28 18:20:44 +0300

    add ...authRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
+import { authRoutes } from 'src/app/features/auth/auth.routes';
+
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
+  ...authRoutes,
   {
     path: '',
     redirectTo: 'home',
