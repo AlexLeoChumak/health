f3d79bdd87462c29dee7e05129e401264ab64938
AlexLeoChumak <chumak1106@gmail.com>
2024-10-02 19:21:45 +0300

    edit registration(userData: PatientRequestInterface | DoctorRequestInterface)

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,11 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, switchMap } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { DoctorRequestInterface } from 'src/app/shared/models/doctor.interface';
 import { PatientRequestInterface } from 'src/app/shared/models/patient.interface';
-import { AuthMessageResponseInterface } from 'src/app/features/auth/models/auth-message-response.interface';
+import { RegistrationResponseInterface } from 'src/app/features/auth/models/registration-response.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -22,10 +22,37 @@ export class AuthService {
 
   registration(
     userData: PatientRequestInterface | DoctorRequestInterface
-  ): Observable<AuthMessageResponseInterface> {
+  ): Observable<RegistrationResponseInterface> {
     const url = `${environment.apiBaseUrl}/${this.registrationBaseUrl}/${
       this.isDoctor(userData) ? 'doctor' : 'patient'
     }`;
-    return this.http.post<AuthMessageResponseInterface>(url, userData);
+
+    const photo = userData?.user?.personalInfo?.photo;
+    const requestData = {
+      ...userData,
+      user: {
+        ...userData.user,
+        personalInfo: { ...userData.user.personalInfo, photo: null },
+      },
+    };
+
+    return this.http.post<RegistrationResponseInterface>(url, requestData).pipe(
+      switchMap((res) => {
+        const userId = res?.userId;
+
+        if (userId && photo && photo instanceof File) {
+          const uploadPhotoUrl = `${url}/${userId}/upload-photo`;
+          const formData = new FormData();
+          formData.append('photo', photo);
+
+          return this.http.put<RegistrationResponseInterface>(
+            uploadPhotoUrl,
+            formData
+          );
+        }
+
+        return of(res);
+      })
+    );
   }
 }
