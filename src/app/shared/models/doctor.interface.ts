88a07c12a463c234dc440b859b2b366d683a6967
AlexLeoChumak <chumak1106@gmail.com>
2024-11-23 16:55:41 +0300

    edit house and housing and apartment to number type

--- src/app/shared/models/doctor.interface.ts
+++ src/app/shared/models/doctor.interface.ts
@@ -1,44 +1,39 @@
-import { PatientBaseInterface } from 'src/app/shared/models/patient.interface';
+import { UserBaseInterface } from 'src/app/shared/models/patient.interface';
 
-interface DoctorBaseInterface extends PatientBaseInterface {
-  user: PatientBaseInterface['user'] & {
-    addressMedicalInstitutionInfo: {
-      apartment: string | null;
-      city: string;
-      district: string;
-      house: string;
-      housing: string | null;
-      region: string;
-      street: string;
-    };
-    educationMedicalWorkerInfo: {
-      faculty: string;
-      licenseNumberMedicalActivities: string;
-      nameEducationalInstitution: string;
-      numberDiplomaHigherMedicalEducation: string;
-      specialistCertificateNumber: string;
-      speciality: string;
-      specialization: string;
-    };
-    placeWorkInfo: {
-      currentSpecialization: string;
-      department: string;
-      nameMedicalInstitution: string;
-    };
+interface DoctorBaseInterface extends UserBaseInterface {
+  addressMedicalInstitutionInfo: {
+    city: string;
+    district: string;
+    house: number;
+    housing: number | null;
+    region: string;
+    street: string;
+  };
+  educationMedicalWorkerInfo: {
+    faculty: string;
+    licenseNumberMedicalActivities: string;
+    nameEducationalInstitution: string;
+    numberDiplomaHigherMedicalEducation: string;
+    specialistCertificateNumber: string;
+    speciality: string;
+    specialization: string;
+  };
+  placeWorkInfo: {
+    currentSpecialization: string;
+    department: string;
+    nameMedicalInstitution: string;
   };
 }
 
-export interface DoctorRequestInterface extends DoctorBaseInterface {
-  user: DoctorBaseInterface['user'] & {
-    contactInfo: DoctorBaseInterface['user']['contactInfo'] & {
+export interface DoctorRequestInterface {
+  user: DoctorBaseInterface & {
+    contactInfo: DoctorBaseInterface['contactInfo'] & {
       password: string;
     };
   };
 }
 
-// export interface DoctorResponseInterface extends DoctorBaseInterface {
-//   id: string;
-//   contactInfo: DoctorBaseInterface['user']['contactInfo'] & {
-//     hashedPassword: string;
-//   };
-// }
+export interface DoctorResponseInterface {
+  id: string;
+  user: DoctorBaseInterface;
+}
--- src/app/shared/models/patient.interface.ts
+++ src/app/shared/models/patient.interface.ts
@@ -1,62 +1,60 @@
-export interface PatientBaseInterface {
-  user: {
-    addressRegistrationInfo: {
-      apartment: string | null;
-      city: string;
-      district: string;
-      house: string;
-      housing: string | null;
-      region: string;
-      street: string;
-    };
-    addressResidenceInfo: {
-      apartment: string | null;
-      city: string;
-      district: string;
-      house: string;
-      housing: string | null;
-      region: string;
-      street: string;
-    };
-    contactInfo: {
-      email: string;
-      homePhoneNumber: string | null;
-      mobilePhoneNumber: string;
-    };
-    identificationInfo: {
-      documentName: string | null;
-      documentNumber: string | null;
-      healthInsuranceContractNumber: string | null;
-      nameInsuranceCompany: string | null;
-      nameStateForeignCitizen: string | null;
-      passportIssueDate: string | null;
-      passportIssuingAuthority: string | null;
-      passportSeriesNumber: string | null;
-      personalIdentificationNumber: string | null;
-      userCitizenship: string;
-    };
-    personalInfo: {
-      dateOfBirth: string;
-      firstName: string;
-      gender: string;
-      lastName: string;
-      middleName: string | null;
-      photo: File | string | null;
+export interface UserBaseInterface {
+  addressRegistrationInfo: {
+    apartment: number | null;
+    city: string;
+    district: string;
+    house: number;
+    housing: number | null;
+    region: string;
+    street: string;
+  };
+  addressResidenceInfo: {
+    apartment: number | null;
+    city: string;
+    district: string;
+    house: number;
+    housing: number | null;
+    region: string;
+    street: string;
+  };
+  contactInfo: {
+    email: string;
+    homePhoneNumber: string | null;
+    mobilePhoneNumber: string;
+  };
+  identificationInfo: {
+    documentName: string | null;
+    documentNumber: string | null;
+    healthInsuranceContractNumber: string | null;
+    nameInsuranceCompany: string | null;
+    nameStateForeignCitizen: string | null;
+    passportIssueDate: string | null;
+    passportIssuingAuthority: string | null;
+    passportSeriesNumber: string | null;
+    personalIdentificationNumber: string | null;
+    userCitizenship: string;
+  };
+  personalInfo: {
+    dateOfBirth: string;
+    firstName: string;
+    lastName: string;
+    middleName: string | null;
+    gender: string;
+    photo: string | File | null;
+  };
+}
+
+// Интерфейс для запроса пациента с оберткой user
+export interface PatientRequestInterface {
+  user: UserBaseInterface & {
+    contactInfo: UserBaseInterface['contactInfo'] & {
+      password: string; // Добавляем поле password
     };
   };
 }
 
-export interface PatientRequestInterface extends PatientBaseInterface {
-  user: PatientBaseInterface['user'] & {
-    contactInfo: PatientBaseInterface['user']['contactInfo'] & {
-      password: string;
-    };
-  };
+// Интерфейс для ответа пациента с оберткой user
+export interface PatientResponseInterface {
+  id: string;
+  user: UserBaseInterface; // Ответ с пользователем без модификаций
 }
-
-// export interface PatientResponseInterface extends PatientBaseInterface {
-//   id: string;
-//   contactInfo: PatientBaseInterface['user']['contactInfo'] & {
-//     hashedPassword: string;
-//   };
-// }
