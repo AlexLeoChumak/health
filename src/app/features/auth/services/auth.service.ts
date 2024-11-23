ec5f9896fc29510fff626adea1393ea660f7d4fd
AlexLeoChumak <chumak1106@gmail.com>
2024-11-23 16:51:50 +0300

    correct uploadUserPhoto

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,19 +1,19 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { from, Observable, of, switchMap, tap } from 'rxjs';
+import { map, Observable, of, switchMap } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { DoctorRequestInterface } from 'src/app/shared/models/doctor.interface';
 import { PatientRequestInterface } from 'src/app/shared/models/patient.interface';
-import { RegistrationResponseInterface } from 'src/app/features/auth/models/registration-response.interface';
+import { GlobalApiResponseInterface } from 'src/app/features/auth/models/global-api-response.interface';
+import { RegistrationApiResponseInterface } from 'src/app/features/auth/models/registration-api-response.interface';
+import { UpdateResultInterface } from 'src/app/shared/models/update-result.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private http: HttpClient = inject(HttpClient);
-  private readonly registrationBaseUrl: string = 'auth/registration';
-  private readonly userProfileBaseUrl: string = 'user-profile';
 
   private isDoctor(
     userData: PatientRequestInterface | DoctorRequestInterface
@@ -23,9 +23,10 @@ export class AuthService {
 
   registration(
     userData: PatientRequestInterface | DoctorRequestInterface
-  ): Observable<any> {
-    //any
-    const url = `${environment.apiBaseUrl}/${this.registrationBaseUrl}/${
+  ): Observable<GlobalApiResponseInterface<RegistrationApiResponseInterface>> {
+    const registrationBaseUrl: string = 'auth/registration';
+
+    const url = `${environment.apiBaseUrl}/${registrationBaseUrl}/${
       this.isDoctor(userData) ? 'doctor' : 'patient'
     }`;
 
@@ -39,32 +40,44 @@ export class AuthService {
       },
     };
 
-    return this.http.post<RegistrationResponseInterface>(url, requestData).pipe(
-      switchMap((response) => {
-        console.log(response);
-
-        return this.uploadUserPhoto(response, photo, userData);
-      })
-    );
+    return this.http
+      .post<GlobalApiResponseInterface<RegistrationApiResponseInterface>>(
+        url,
+        requestData
+      )
+      .pipe(
+        switchMap((response) => {
+          return this.uploadUserPhoto(response, photo, userData);
+        })
+      );
   }
 
   uploadUserPhoto(
-    response: RegistrationResponseInterface,
+    response: GlobalApiResponseInterface<RegistrationApiResponseInterface>,
     photo: string | File | null,
     userData: PatientRequestInterface | DoctorRequestInterface
-  ) {
+  ): Observable<GlobalApiResponseInterface<RegistrationApiResponseInterface>> {
+    const userProfileBaseUrl: string = 'user-profile';
+    const uploadPhotoBaseUrl: string = 'upload-photo';
+
     const userId = response?.data?.userId;
 
     if (userId && photo && photo instanceof File) {
-      const uploadPhotoUrl = `${environment.apiBaseUrl}/user-profile/${
+      const uploadPhotoFullUrl = `${
+        environment.apiBaseUrl
+      }/${userProfileBaseUrl}/${
         this.isDoctor(userData) ? 'doctor' : 'patient'
-      }/${userId}/upload-photo`;
+      }/${userId}/${uploadPhotoBaseUrl}`;
+
       const formData = new FormData();
       formData.append('photo', photo);
 
       return this.http
-        .patch<any>(uploadPhotoUrl, formData)
-        .pipe(tap((q) => console.log('qq', q))); //any
+        .patch<GlobalApiResponseInterface<UpdateResultInterface>>(
+          uploadPhotoFullUrl,
+          formData
+        )
+        .pipe(map(() => response));
     }
 
     return of(response);
